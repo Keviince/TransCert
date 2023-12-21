fa6cf161d2b50a345bd4ba0750b1e9a102df500a
Keviince <32187894+Keviince@users.noreply.github.com>
2023-12-21 15:33:31 +0800

    Add files via upload

---
+++ pull_cert.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# Certificate server address
+CERT_SERVER="protocol://ip:port"
+
+# operations after pulled certificate
+after_pull() {
+	systemctl force-reload nginx
+}
+
+read_params() {
+	CERT_DOMAIN=""
+	CERT_PASSWORD=""
+	CERT_LOCATION=""
+	while [ $# -gt 0 ]; do
+		case "$1" in
+			--domain=*)
+				CERT_DOMAIN="${1#*=}"
+				;;
+			--password=*)
+				CERT_PASSWORD="${1#*=}"
+				;;
+			--location=*)
+				CERT_LOCATION="${1#*=}"
+				;;
+			--help)
+				show_help
+				;;
+			*)
+				echo "Unknown Argument: $1"
+				;;
+		esac
+		shift
+	done
+	if [ -z $CERT_DOMAIN ] || [ -z $CERT_PASSWORD ] || [ -z $CERT_LOCATION ]; then
+		show_help
+	fi
+}
+
+check_cert_update() {
+	# 1: Certificate need to be decompressed
+	# 0: Certificate is up to date
+	local LOCAL_CERT_GPG=$CERT_LOCATION"/"$CERT_DOMAIN".tgz.gpg"
+	if [ -f $LOCAL_CERT_GPG ]; then
+		echo "Local certificate found. Checking SHA256 Hash..."
+		local LOCAL_CERT_SIG=$(sha256sum $LOCAL_CERT_GPG | awk '{print $1}')
+		local REMOTE_CERT_SIG=$CERT_SERVER"/"$CERT_DOMAIN"/"$CERT_DOMAIN".sig"
+		REMOTE_CERT_SIG=$(curl --silent $REMOTE_CERT_SIG)
+		if [ $LOCAL_CERT_SIG = $REMOTE_CERT_SIG ]; then
+			echo "Local certificate is up to date."
+			return 0
+		else
+			echo "Local certificate is outdated. Downloading..."
+			local REMOTE_CERT_GPG=$CERT_SERVER"/"$CERT_DOMAIN"/"$CERT_DOMAIN".tgz.gpg"
+			curl --silent $REMOTE_CERT_GPG -o $LOCAL_CERT_GPG
+			return 1
+		fi
+	else
+		echo "No local certificate found. Downloading..."
+		local REMOTE_CERT_GPG=$CERT_SERVER"/"$CERT_DOMAIN"/"$CERT_DOMAIN".tgz.gpg"
+		curl --silent $REMOTE_CERT_GPG -o $LOCAL_CERT_GPG
+		return 1
+	fi
+}
+
+cert_decompress() {
+	echo "Decompressing certificate..."
+	local LOCAL_CERT_GPG=$CERT_LOCATION"/"$CERT_DOMAIN".tgz.gpg"
+	gpg --batch --yes --passphrase $CERT_PASSWORD -q -d $LOCAL_CERT_GPG | tar xz -C $CERT_LOCATION
+}
+
+show_help() {
+  echo "Usage: sh $0 --domain=[Domain] --password=[Password] --location=[Location]"
+  echo "Example:"
+  echo "--domain=www.keviince.com	Domain Name"
+  echo "--password=ExamplePWD		Decompress Password"
+  echo "--location=/etc/nginx/cert	Certificate Home"
+  echo "--help				Show Help"
+  exit 0
+}
+
+main() {
+	read_params "$@"
+	echo "Start pulling "$CERT_DOMAIN"..."
+	check_cert_update
+	if [ $? -eq 1 ]; then
+		cert_decompress
+	fi
+	after_pull
+	echo "Finish pulling "$CERT_DOMAIN"."
+}
+
+main "$@"
---
+++ push_cert.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# certificate to publish
+# often web server directory
+CERT_HOME="/home/Certs"
+
+# acme.sh script home
+ACME_HOME="/root/.acme.sh"
+
+# encryption password
+CERT_PASSWORD="YourEncryptionPassword"
+
+CERT_ARRAY=$($ACME_HOME/acme.sh --list | awk '{print $1}' | sed '1d')
+CERT_COUNT=$($ACME_HOME/acme.sh --list | awk '{print $1}' | sed '1d' | wc -l)
+
+# push your certificate(s) to web server directory
+push_cert() {
+        echo "Start pushing "$1"..."
+        local CERT_TARGET=$CERT_HOME"/"$1
+        mkdir -p $CERT_TARGET
+        local CERT_DOMAIN=$1
+        local CERT_LOCATION=$ACME_HOME"/"$CERT_DOMAIN"_ecc/"
+        local CERT_FULLCHAIN=$CERT_LOCATION"fullchain.cer"
+        local CERT_PEM=$CERT_LOCATION$CERT_DOMAIN".pem"
+        local CERT_KEY=$CERT_LOCATION$CERT_DOMAIN".key"
+        cp -f $CERT_FULLCHAIN $CERT_PEM
+        local CERT_TGZ=$CERT_HOME"/"$CERT_DOMAIN"/"$CERT_DOMAIN".tgz"
+        local CERT_GPG=$CERT_TGZ".gpg"
+        local CERT_SIG=$CERT_HOME"/"$CERT_DOMAIN"/"$CERT_DOMAIN".sig"
+        tar -czPf $CERT_TGZ --transform "s,^.*/.*/,,g" $CERT_PEM $CERT_KEY
+        gpg --batch --yes --passphrase $2 -q -c $CERT_TGZ
+        sha256sum $CERT_GPG | awk '{print $1}' > $CERT_SIG
+        rm -f $CERT_TGZ $CERT_PEM
+        echo "Finish pushing "$1"..."
+        return 0
+}
+
+check_cert_count() {
+        if [ $CERT_COUNT -eq 0 ]; then
+                echo "No certificate found."
+                exit 1
+        else
+                echo $CERT_COUNT" certificate(s) found. Start pushing..."
+        fi   
+}
+
+main() {
+        check_cert_count
+        for CERT in $CERT_ARRAY
+        do
+                push_cert $CERT $CERT_PASSWORD
+        done
+}
+
+main
